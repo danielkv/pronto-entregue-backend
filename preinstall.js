2169f640b11e6342691ba486497a0b74e37eab73
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-01-13 19:05:05 -0300

    init

--- preinstall.js
+++ preinstall.js
@@ -1,5 +1,7 @@
 var fs=require('fs');
 
+if (!process.env.GCP_KEY_FILE || !process.env.GCP_CRED) return;
+
 fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {
 	if (err) throw err;
 });
