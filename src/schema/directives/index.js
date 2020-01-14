4efda7ed32eaaef75373ab6532f8a01da58056bd
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-01-14 20:17:16 -0300

    remove directive dateTime

--- src/schema/directives/index.js
+++ src/schema/directives/index.js
@@ -15,20 +15,5 @@ export default {
 		if (!(ctx.user instanceof User)) throw new Error('Usuário não autenticado')
 		
 		return next();
-	},
-	dateTime: (_, source) => {
-		const date = new Date(source.get('createdAt'));
-		let day = date.getDate();
-		let month = date.getMonth()+1;
-		let year = date.getFullYear();
-		let hours = date.getHours();
-		let minutes = date.getMinutes();
-
-		if (day < 10) day = `0${day}`;
-		if (month < 10) month = `0${month}`;
-		if (hours < 10) hours = `0${hours}`;
-		if (minutes < 10) minutes = `0${minutes}`;
-
-		return `${day}/${month}/${year} ${hours}: {minutes}`;
 	}
 }
