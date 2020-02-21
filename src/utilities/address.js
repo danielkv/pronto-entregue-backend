b8fdb36499001ddf5895321f1f4b1da9a2711476
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-02-20 21:45:25 -0300

    fixed search address

--- src/utilities/address.js
+++ src/utilities/address.js
@@ -1,4 +1,6 @@
 function parseAddressesComponents(results) {
+	if (!results.length) return [];
+
 	return results.map(result => {
 		const componentTypes = {
 			location: [result.geometry.location.lat, result.geometry.location.lng]
@@ -14,12 +16,12 @@ function parseAddressesComponents(results) {
 
 export function parseAddresses(results) {
 	const addresses = parseAddressesComponents(results).map(comp => ({
-		street: comp.route.long_name,
-		number: comp.street_number.long_name ? parseInt(comp.street_number.long_name) : null,
-		zipcode: comp.postal_code.long_name ? parseInt(comp.postal_code.long_name.replace('-', '')) : null,
-		district: comp.sublocality.long_name || comp.sublocality_level_1.long_name,
-		city: comp.administrative_area_level_2.long_name,
-		state: comp.administrative_area_level_1.short_name,
+		street: comp.route ? comp.route.long_name : null,
+		number: comp.street_number ? parseInt(comp.street_number.long_name) : null,
+		zipcode: comp.postal_code ? parseInt(comp.postal_code.long_name.replace('-', '')) : null,
+		district: comp.sublocality ? comp.sublocality.long_name : comp.sublocality_level_1 ? comp.sublocality_level_1.long_name : null,
+		city: comp.administrative_area_level_2 ? comp.administrative_area_level_2.long_name : null,
+		state: comp.administrative_area_level_1 ? comp.administrative_area_level_1.short_name : null,
 		location: { type: 'Point', coordinates: comp.location },
 	}))
 
