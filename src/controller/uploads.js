c26b19d816323f4aa8ea141c3beebfab99b1225f
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-04-09 19:00:14 -0300

    resize images

--- src/controller/uploads.js
+++ src/controller/uploads.js
@@ -1,24 +1,26 @@
 import crypto  from 'crypto';
 import { extname, basename }  from 'path';
+import sharp from 'sharp';
 
 import { storage }  from '../services/gcloud';
 import { slugify }  from '../utilities';
 
+
 export const upload = async (bucketName, file) => {
 	const bucket = await getFileBucket(bucketName);
 	const { url } = await startUpload(bucket, file);
-
+	
 	return url;
 }
 
 const getFileBucket = async (bucketName) => {
 	const slugifiedName = `${slugify(bucketName)}_flakery`;
 	const bucket = storage.bucket(slugifiedName);
-
+	
 	return bucket.exists()
 		.then(async ([exists])=>{
 			if (exists) return bucket;
-
+		
 			const [createdBucket] = await storage.createBucket(slugifiedName);
 			return createdBucket;
 		})
@@ -29,14 +31,18 @@ const newFileName = (fileName, bytes=16) => {
 	const baseName = basename(fileName, fileExtension);
 	const hash = crypto.randomBytes(bytes);
 	const newName = `${slugify(baseName)}-${hash.toString("hex")}${fileExtension}`;
-
+	
 	return newName;
 }
+
+const imageResizer = sharp()
+	.resize({ width: 1080, fit: 'inside', withoutEnlargement: true })
+	.jpeg({ quality: 93 });
 
 const startUpload = async (bucket, file) => {
 	return new Promise((resolve, reject)=>{
 		const { filename, createReadStream, mimetype } = file;
-
+		
 		const newFile = bucket.file(newFileName(filename));
 		const writeStream = newFile.createWriteStream({
 			resumable: false,
@@ -46,14 +52,14 @@ const startUpload = async (bucket, file) => {
 				contentType: mimetype
 			}
 		})
-
+		
 		createReadStream()
-			
+			.pipe(imageResizer)
 			.pipe(writeStream)
 			.on('error', reject)
 			.on('finish', ()=>{
 				const url = `https://${newFile.storage.apiEndpoint}/${newFile.bucket.name}/${newFile.name}`;
-				
+			
 				resolve({ file: newFile, url });
 			});
 	});
