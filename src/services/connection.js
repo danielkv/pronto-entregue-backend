16955e02a798284aedba82bde954ee9bf2743398
Daniel K. Guolo <daniel_kv@hotmail.com>
2020-04-21 16:05:07 -0300

    max_connections

--- src/services/connection.js
+++ src/services/connection.js
@@ -4,8 +4,8 @@ export default new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, proce
 	host: process.env.MYSQL_HOST,
 	dialect: 'mysql',
 	pool: {
-		max: 30,
+		max: 100,
 		min: 0,
-		idle: 10000
+		idle: 500
 	},
 });
